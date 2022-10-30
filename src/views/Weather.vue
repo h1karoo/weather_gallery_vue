9cbbd41bb3530f1c1827535f5d6a2d7e1a89f6cf
h1karoo <o.filipo@yandex.ru>
2022-10-31 05:52:31 +0700

    fixed

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id = "app2" :class= "typeof weather.main != 'undefined' &&  weather.main.temp > 16 ? 
+  <div id = "app2" :class= "typeof weather.main != 'undefined' &&  weather.main.temp - 273 > 16 ? 
     'warm': '' ">
     
     <main>
@@ -19,7 +19,7 @@
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}°С</div>
+          <div class="temp">{{ Math.round(weather.main.temp - 273) }}°С</div>
           <div class="weather">
             <img id="icon" :src="`http://openweathermap.org/img/wn/` + weather.weather[0].icon + `@2x.png`"/>
             </div>
@@ -35,7 +35,7 @@ export default {
   data () {
     return {
       api_key:'5a4d2158666e70ee88bd299dcc12ab40',
-      url_base: 'https://api.openweathermap.org/data/2.5/',
+      url_base: 'http://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {},
       
@@ -46,7 +46,7 @@ export default {
   methods: {
     fetchWeather (e) {
       if (e.key == "Enter") {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        fetch(`${this.url_base}weather?q=${this.query}&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
           }).then(this.setResults);
